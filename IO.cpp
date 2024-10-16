92a677fa175ef6071bc965cc592d3ceb2fdc5ba5
Madeira <leandromad140@hotmail.com>
2024-10-16 12:30:16 -0400

    interface inicial
    implementação da interface protoboard

---
+++ IO.cpp
@@ -0,0 +1,11 @@
+#include "IO.h"
+#include <iostream>
+
+// void IO::setEntrada(int bits[8]) {
+//     for (int i = 0; i < 8; i++)
+//     {
+//         entrada[i] = bits[i];
+//     }
+    
+// }
+
