6b2400d12919b454c1122f9f37ab293dee5e0a61
Madeira <leandromad140@hotmail.com>
2024-11-01 19:15:09 -0400

    Modificação classes (sem testar)
    união das classes simples junto da classe base Componentes

--- IO.cpp
+++ IO.cpp
@@ -1,26 +1,27 @@
 #include "IO.h"
 
 IO::IO() {
-    for (int i = 0; i<8; i++)
-        entrada.push_back(0);
+    for (int i = 0; i < 8; i++) {
+        bitsEntrada[i] = 0;
+        bitsSaida[i] = 0;
+    }
 }
 
 IO::~IO() {
-    for (int i = 0; i<8; i++)
-        entrada.pop_back();
+
 }
-void IO::setEntrada(const vector<int> &bits) {
-    entrada = bits;
+void IO::setEntrada(int bit, int indice) {
+    bitsEntrada[indice] = bit;
 }
 
-void IO::setSaida(const vector<int> &bits) {
-    saida = bits;
+void IO::setSaida(int bit, int indice) {
+    bitsSaida[indice] = bit;
 }
 
-vector<int> IO::getEntrada() const{
-    return entrada;
+int IO::getEntrada(int indice) const{
+    return bitsEntrada[indice];
 }
 
-vector<int> IO::getSaida() const {
-    return  saida;
+int IO::getSaida(int indice) const {
+    return  bitsSaida[indice];
 }
