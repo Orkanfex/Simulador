470aacce4a6bfacc7e409e0c38fe388cdcc45bbf
Gabriel <gabriel306azevedo@gmail.com>
2024-11-03 18:26:29 -0400

    Ajuste da fonte que faltou o &

--- AppSimulador.cpp
+++ AppSimulador.cpp
@@ -1,13 +1,7 @@
-//
-// Created by madeira on 02/11/24.
-//
-
 #include "AppSimulador.h"
 #include <iostream>
 
-
 AppSimulador::AppSimulador() {
-
     for (int i = 0; i < 8; i++) {
         input.push_back(0);
         output.push_back(0);
@@ -18,37 +12,77 @@ void AppSimulador::registraComponente(ComponentesApp * pComponente) {
     registroDeComponentes.push_back(pComponente->alocarComponenteApp());
 }
 
+// void AppSimulador::plotProtoboard(RenderWindow &window, vector<CircleShape>& conector) {
+//     // Cria a borda da protoboard
+//     RectangleShape protoboardBorda(Vector2f(400.f, 400.f));
+//     protoboardBorda.setOutlineColor(Color::Black);
+//     protoboardBorda.setOutlineThickness(5.f);
+//     protoboardBorda.setPosition(205, 150); // Ajuste a posição conforme necessário
+//
+//     float x, y;
+//
+//     conector.clear();
+//
+//     // Calcula a posição dos conectores
+//     x = INIT_PROTO_X;
+//     for (int i = 0; i < 8; i++) {
+//         y = INIT_PROTO_Y + 20;
+//         for (int j = 0; j < 8; j++) {
+//             CircleShape shape(5.f);
+//             shape.setFillColor(Color::Black);
+//             shape.setPosition(x, y); // Define a posição do conector
+//             conector.push_back(shape); // Adiciona o conector ao vetor
+//
+//             y += PADDING_CONCTOR; // Move para a próxima linha
+//         }
+//         x += PADDING_CONCTOR; // Move para a próxima coluna
+//     }
+//
+//     // Desenha a borda da protoboard e os conectores
+//     window.draw(protoboardBorda);
+//     for (auto &shape : conector) {
+//         window.draw(shape);
+//     }
+// }
+
 void AppSimulador::mainDoApp() {
     RenderWindow window(VideoMode(800, 800), "Simulador Lógico");
     vector<RectangleShape> botoes;
     vector<Text> bitsEntrada, bitsSaida;
     vector<BUttonComponentApp> painel;
+    vector<CircleShape> conector;
     Font font;
 
-    if (!font.loadFromFile("../Fontes/ARIAL.TTF")) {
+    if (!font.loadFromFile("../Fontes/arial.ttf")) {
         std::cerr << "Erro ao carregar a fonte!" << endl;
     }
 
     configureButtonsInput(botoes, font, bitsEntrada);
     painelComponentes(window, font, painel);
+    //plotProtoboard(window, conector);
 
     while (window.isOpen()) {
-        Event event;
+        Event event{};
 
         while (window.pollEvent(event)) {
             if (event.type == Event::Closed) {
                 window.close();
             }
+
             if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
                 Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
 
+                // Verifica se clicou em algum botão de entrada e altera seu estado
                 for (int i = 0; i < botoes.size(); ++i) {
                     if (botoes[i].getGlobalBounds().contains(mousePos)) {
                         input[i] = !input[i];
                     }
                 }
             }
+
         }
+
+        //transferirValores();
 
         window.clear(Color::White);
 
@@ -59,8 +93,7 @@ void AppSimulador::mainDoApp() {
         window.display();
     }
 }
-void AppSimulador::configureButtonsInput(vector<RectangleShape> &botoes,
-    Font fonte, vector<Text> &text) {
+void AppSimulador::configureButtonsInput(vector<RectangleShape> &botoes, Font &fonte, vector<Text> &text) const {
     for (int i = 0; i < 8; i++) {
         RectangleShape botao(Vector2f(20.f, 20.f));
         botoes.push_back(botao);
@@ -74,14 +107,14 @@ void AppSimulador::configureButtonsInput(vector<RectangleShape> &botoes,
 }
 
 void AppSimulador::plotEntradaBIts(RenderWindow &window,
-    vector<RectangleShape> &botoes, Font font) {
+    vector<RectangleShape> &botoes, Font &font) const {
 
-    int x, y;
+    float x, y;
     RectangleShape entradaDados(Vector2f(SIZE_PROTOBOARD / 1.4f, 30.f));
     vector<Text> bitsTxt;
 
     y = INIT_PROTO_Y + SIZE_PROTOBOARD + (PADDING_CONCTOR / 2);
-    x = INIT_PROTO_X - (PADDING_CONCTOR / 2) + (PADDING_CONCTOR * 1.6);
+    x = INIT_PROTO_X - PADDING_CONCTOR / 2 + (PADDING_CONCTOR * 1.6);
 
     entradaDados.setOutlineThickness(5.f);
     entradaDados.setOutlineColor(Color::Black);
@@ -91,7 +124,7 @@ void AppSimulador::plotEntradaBIts(RenderWindow &window,
 
     vector<RectangleShape>::iterator it;
     int j = 0;
-    for (it = botoes.begin(); it < botoes.end(); it++, j++) {
+    for (it = botoes.begin(); it < botoes.end(); ++it, j++) {
         CircleShape shape(5.f);
         shape.setFillColor(Color::Black);
         shape.setPosition(x, y);
@@ -114,8 +147,8 @@ void AppSimulador::plotEntradaBIts(RenderWindow &window,
 
 }
 
-void AppSimulador::plotSaidaBits(RenderWindow &window, Font fonte) {
-    int x, y;
+void AppSimulador::plotSaidaBits(RenderWindow &window, Font &fonte) const {
+    float x, y;
     RectangleShape saidaDados(Vector2f(SIZE_PROTOBOARD / 1.4f, 30.f));
     vector<Text> bitsTxt;
 
@@ -136,7 +169,7 @@ void AppSimulador::plotSaidaBits(RenderWindow &window, Font fonte) {
         bitText.setCharacterSize(20);
         bitText.setString(to_string(output[j]));
         bitText.setFillColor(Color::Black);
-        bitText.setPosition(x + (PADDING_CONCTOR / 15), y + 30);
+        bitText.setPosition(x + PADDING_CONCTOR / 15, y + 30);
 
         window.draw(bitText);
 
@@ -144,21 +177,20 @@ void AppSimulador::plotSaidaBits(RenderWindow &window, Font fonte) {
         shape.setFillColor(Color::Black);
         shape.setPosition(x, y + 65);
         window.draw(shape);
-        x += (PADDING_CONCTOR / 1.5);
+        x += PADDING_CONCTOR / 1.5;
     }
 }
 
-void AppSimulador::painelComponentes(RenderWindow &window, Font font, vector<BUttonComponentApp> &painel) {
+void AppSimulador::painelComponentes(RenderWindow &window, const Font &font, vector<BUttonComponentApp> &painel) {
     vector<ComponentesApp*>::iterator it;
     Vector2f position;
     position.x = INI_COLUM_PAINEL;
     position.y = INI_LINHA_PAINEL;
 
-    for (it = registroDeComponentes.begin(); it < registroDeComponentes.end(); it++) {
+    for (it = registroDeComponentes.begin(); it < registroDeComponentes.end(); ++it) {
 
         BUttonComponentApp botao(position,font,(*it)->getNome());
         painel.push_back(botao);
-
         position.y += 50;
 
     }
@@ -167,10 +199,24 @@ void AppSimulador::painelComponentes(RenderWindow &window, Font font, vector<BUt
 void AppSimulador::drawPainel(RenderWindow &window, vector<BUttonComponentApp> &painel) {
     vector<BUttonComponentApp>::iterator it;
 
-    for (it = painel.begin(); it < painel.end(); it++) {
-        (*it).draw(window);
+    for (it = painel.begin(); it < painel.end(); ++it) {
+        it->draw(window);
     }
 }
 
-AppSimulador::~AppSimulador() {
-}
+AppSimulador::~AppSimulador() = default;
+
+///////////////////////////////////////////////////////////////
+// void AppSimulador::transferirValores() {
+//     // conect.push_back({0, 1}); // Conecta a entrada 0 com saída 3
+//     // conect.push_back({1, 2}); // Conecta a entrada 1 com saída 2
+//     // conect.push_back({2, 3}); // Conecta a entrada 2 com saída 5
+//
+//     for (const Conect& conexao : conect) {
+//         output[conexao.posSaida] = input[conexao.posEntrada];
+//     }
+//
+// }
+
+
+
--- AppSimulador.h
+++ AppSimulador.h
@@ -1,7 +1,3 @@
-//
-// Created by madeira on 02/11/24.
-//
-
 #ifndef APPSIMULADOR_H
 #define APPSIMULADOR_H
 
@@ -11,8 +7,8 @@
 #include "BUttonComponentApp.h"
 #include  <SFML/Graphics.hpp>
 
-#define INI_LINHA_PAINEL 100;
-#define INI_COLUM_PAINEL 30;
+#define INI_LINHA_PAINEL 100
+#define INI_COLUM_PAINEL 30
 #define INIT_PROTO_X 225
 #define INIT_PROTO_Y 150
 #define SIZE_PROTOBOARD 400
@@ -21,26 +17,37 @@
 using namespace sf;
 
 class AppSimulador {
+
+    struct Conect {
+        int posEntrada;
+        int posSaida;
+    };
+    vector<Conect> conect;
+
     vector<ComponentesApp*> registroDeComponentes;
     CalcLogico simular;
-    int board[8][8];
+    int board[8][8]{};
     vector<int> input;
     vector<int> output;
 
     RectangleShape shape;
     Text label;
-    bool isDragging;
+    bool isDragging{};
 
 public:
     AppSimulador();
     void registraComponente(ComponentesApp*);
     void mainDoApp();
-    void configureButtonsInput(vector<RectangleShape> &botoes, Font fonte, vector<Text> &text);
-    void plotEntradaBIts(RenderWindow &windows, vector<RectangleShape> &botoes, Font font);
-    void plotSaidaBits(RenderWindow &window, Font font);
-    void painelComponentes(RenderWindow &window, Font font, vector<BUttonComponentApp> &painel);
-    void drawPainel(RenderWindow &window, vector<BUttonComponentApp> &painel);
+    void configureButtonsInput(vector<RectangleShape> &botoes, Font &fonte, vector<Text> &text) const;
+    void plotEntradaBIts(RenderWindow &windows, vector<RectangleShape> &botoes, Font &font) const;
+    void plotSaidaBits(RenderWindow &window, Font &font) const;
+    void painelComponentes(RenderWindow &window, const Font &font, vector<BUttonComponentApp> &painel);
+
+    static void drawPainel(RenderWindow &window, vector<BUttonComponentApp> &painel);
     ~AppSimulador();
+
+    // void transferirValores();
+    // static void plotProtoboard(RenderWindow &window, vector<CircleShape>& conector);
 };
 
 
